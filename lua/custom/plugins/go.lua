9c5cf186ddd6416489835e9d8f445e72a7c7e0a2
ixpectus <ixpectus@gmail.com>
2024-07-22 15:46:23 +0300

    add go.nvim

--- lua/custom/plugins/go.lua
+++ lua/custom/plugins/go.lua
@@ -8,7 +8,7 @@ return {
   },
   config = function()
     require('go').setup()
-    -- vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
+    vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
   end,
   event = { 'CmdlineEnter' },
   ft = { 'go', 'gomod' },
