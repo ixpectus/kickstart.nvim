70ae2e2a8266e5418d50e79499813a49cd7c7a9c
ixpectus <ixpectus@gmail.com>
2026-08-21 18:41:26 +0300

    add pi sessions in telescope

--- lua/custom/herdr/telescope_sessions.lua
+++ lua/custom/herdr/telescope_sessions.lua
@@ -37,7 +37,7 @@ function M.find()
       date_str = dt:sub(1, 19):gsub('T', ' ')
     end
 
-    local display = string.format('%s  msgs:%d  %s  %s', s.id:sub(1, 8), s.message_count, date_str, s.duration or '-')
+    local display = string.format('%d %s %s %s ', s.message_count, s.duration or '-', date_str, s.id:sub(1, 8))
     table.insert(entries, {
       id = s.id,
       file = s.file,
@@ -56,73 +56,78 @@ function M.find()
     return
   end
 
-  pickers.new({}, themes.get_dropdown {
-    prompt_title = 'Pi Sessions',
-    sorting_strategy = 'ascending',
-    layout_config = {
-      height = 14,
-      width = 0.9,
-      mirror = true,
-    },
-    finder = require('telescope.finders').new_table {
-      results = entries,
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = entry.display,
-          ordinal = entry.ordinal,
-          file = entry.file,
-        }
-      end,
-    },
-    sorter = conf.generic_sorter(),
-    previewer = previewers.new_buffer_previewer {
-      title = 'First prompt',
-      dyn_title = function(_, entry)
-        if not entry or not entry.value then
-          return ''
-        end
-        local p = entry.value.first_prompt or ''
-        return #p > 80 and p:sub(1, 80) .. '…' or p
-      end,
-      define_preview = function(self, entry)
-        if not entry or not entry.value then
-          return
-        end
-        local prompt = entry.value.first_prompt or ''
-        local lines = vim.split(prompt, '\n')
-        -- Обрезаем превью до 20 строк
-        if #lines > 20 then
-          lines = vim.list_slice(lines, 1, 20)
-          table.insert(lines, '…')
-        end
-        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
-        vim.wo[self.state.winid].wrap = true
-      end,
-    },
-    attach_mappings = function(prompt_bufnr)
-      -- Открыть файл сессии
-      actions.select_default:replace(function()
-        local selection = action_state.get_selected_entry()
-        actions.close(prompt_bufnr)
-        local file = selection.value.file
-        if file then
-          vim.cmd('edit ' .. vim.fn.fnameescape(file))
-        end
-      end)
+  pickers
+    .new(
+      {},
+      themes.get_dropdown {
+        prompt_title = 'Pi Sessions',
+        sorting_strategy = 'ascending',
+        layout_config = {
+          height = 14,
+          width = 0.9,
+          mirror = true,
+        },
+        finder = require('telescope.finders').new_table {
+          results = entries,
+          entry_maker = function(entry)
+            return {
+              value = entry,
+              display = entry.display,
+              ordinal = entry.ordinal,
+              file = entry.file,
+            }
+          end,
+        },
+        sorter = conf.generic_sorter(),
+        previewer = previewers.new_buffer_previewer {
+          title = 'First prompt',
+          dyn_title = function(_, entry)
+            if not entry or not entry.value then
+              return ''
+            end
+            local p = entry.value.first_prompt or ''
+            return #p > 80 and p:sub(1, 80) .. '…' or p
+          end,
+          define_preview = function(self, entry)
+            if not entry or not entry.value then
+              return
+            end
+            local prompt = entry.value.first_prompt or ''
+            local lines = vim.split(prompt, '\n')
+            -- Обрезаем превью до 20 строк
+            if #lines > 20 then
+              lines = vim.list_slice(lines, 1, 20)
+              table.insert(lines, '…')
+            end
+            vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+            vim.wo[self.state.winid].wrap = true
+          end,
+        },
+        attach_mappings = function(prompt_bufnr)
+          -- Открыть файл сессии
+          actions.select_default:replace(function()
+            local selection = action_state.get_selected_entry()
+            actions.close(prompt_bufnr)
+            local file = selection.value.file
+            if file then
+              vim.cmd('edit ' .. vim.fn.fnameescape(file))
+            end
+          end)
 
-      -- Скопировать id сессии
-      vim.keymap.set('n', 'y', function()
-        local selection = action_state.get_selected_entry()
-        if selection and selection.value.id then
-          vim.fn.setreg('+', selection.value.id)
-          vim.notify('Session ID скопирован: ' .. selection.value.id, vim.log.levels.INFO)
-        end
-      end, { buffer = prompt_bufnr, silent = true, desc = 'Copy session ID' })
+          -- Скопировать id сессии
+          vim.keymap.set('n', 'y', function()
+            local selection = action_state.get_selected_entry()
+            if selection and selection.value.id then
+              vim.fn.setreg('+', selection.value.id)
+              vim.notify('Session ID скопирован: ' .. selection.value.id, vim.log.levels.INFO)
+            end
+          end, { buffer = prompt_bufnr, silent = true, desc = 'Copy session ID' })
 
-      return true
-    end,
-  }):find()
+          return true
+        end,
+      }
+    )
+    :find()
 end
 
 return M
